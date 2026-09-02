7b99368151298893ab63279d327f760d433791d9
Phuoc <23521228@gm.uit.edu.vn>
2026-09-02 13:37:23 +0700

    useIsFocused và listeners và bla blaa

--- src/screens/HomeTabs/Feed.tsx
+++ src/screens/HomeTabs/Feed.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useIsFocused } from '@react-navigation/native';
 
 export default function Feed() {
+    const isFocus=useIsFocused();
+    console.log(isFocus? 'feed active':"");
     return (
         <View>
             <Text>Feed</Text>
--- src/screens/HomeTabs/HomeTab.tsx
+++ src/screens/HomeTabs/HomeTab.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useState } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import {
@@ -10,21 +11,30 @@ import Feed from './Feed';
 import FlatlistScreen from '../flatlist';
 const Tab = createBottomTabNavigator();
 export default function HomeTab() {
+    const [currentTab,setCurrentTab]=useState("feed");
+    console.log(currentTab);
     return (
         <Tab.Navigator screenOptions={{ headerShown: false, tabBarActiveTintColor:"red" }}>
-            <Tab.Screen name="Feed" component={Feed} options={{
+            <Tab.Screen name="Feed"
+             component={Feed} options={{
                 title: "Tin mới",
-                tabBarIcon: ()=>{
+                tabBarIcon: ({color})=>{
                     return (
-                        <Ionicons name="albums-sharp" size={24} color="black" />
+                        <Ionicons name="albums-sharp"  size={24} color= {color} />
                         )
                 }
-            }} />
+            }}
+            listeners={{
+                        tabPress:()=>{
+                            setCurrentTab("feed")
+                        }
+            }}
+            />
             <Tab.Screen name="flatlist" component={FlatlistScreen}
                 options={{ title: "Danh sách sản phẩm" ,
-                    tabBarIcon:()=>{
-                        return(<Ionicons name="aperture" size={24} color="blue" 
-                        
+                    tabBarIcon:({color})=>{
+                        return(<Ionicons name="aperture" size={24} color={color} 
+                            
                         />) 
                     },
                     tabBarBadge:3,
